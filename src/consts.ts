26c93a7aaac1b0874df37f301ee3d2ff636199ca
J <jorcallegar@gmail.com>
2025-07-14 21:18:19 +0200

    1st

---
+++ src/consts.ts
@@ -0,0 +1,12 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'thejo114ge - Web Page';
+export const SITE_DESCRIPTION = 'Security Engineer | Cloud Security Specialist | Incident Response Expert';
+export const PORTFOLIO_NAME = 'theJo114ge';
+export const PORTFOLIO_ROLE = 'Security Engineer';
+export const PORTFOLIO_LOCATION = 'Spain';
+export const PORTFOLIO_EMAIL = 'thejo114ge@gmail.com';
+export const PORTFOLIO_GITHUB = 'https://github.com/jo114ge';
+export const PORTFOLIO_LINKEDIN = 'https://linkedin.com/in/jorge-calleja';
+export const PORTFOLIO_CREDLY = 'https://www.credly.com/users/jorge-calleja';
